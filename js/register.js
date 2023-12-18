f3e03dfbaf41b535973b1159f6ae68532c02aedc
Almudena Rivera <riveraferalmudena@gmail.com>
2023-12-18 14:01:01 +0100

    registro validaciones

--- js/register.js
+++ js/register.js
@@ -48,7 +48,25 @@ inputs.forEach((input) => {
 	input.addEventListener('blur', validarFormulario);
 });
 
+formulario.addEventListener('submit', (e) => {
+	e.preventDefault();
 
+	const terminos = document.getElementById('terminos');
+	if(campos.usuario && campos.nombre && campos.password && campos.correo && campos.telefono && terminos.checked ){
+		formulario.reset();
+
+		document.getElementById('formulario__mensaje-exito').classList.add('formulario__mensaje-exito-activo');
+		setTimeout(() => {
+			document.getElementById('formulario__mensaje-exito').classList.remove('formulario__mensaje-exito-activo');
+		}, 5000);
+
+		document.querySelectorAll('.formulario__grupo-correcto').forEach((icono) => {
+			icono.classList.remove('formulario__grupo-correcto');
+		});
+	} else {
+		document.getElementById('formulario__mensaje').classList.add('formulario__mensaje-activo');
+	}
+});
 
 document.addEventListener('DOMContentLoaded', function () {
     var formulario = document.querySelector('.formulario form');
