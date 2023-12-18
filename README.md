28a7756fda077c4808d80c7fba9a8316954c5fd0
Carla Escobar <carlaprimola@outlook.es>
2023-12-18 10:20:04 +0100

    Añadiendo nuevo README

--- README.md
+++ README.md
@@ -1,10 +1,64 @@
-# chichaRrica
-Pagina del superfotografo internacional MM
 
-Hemos creado una pagina web de fotografía, donde están disponibles y a la venta unas fotografías, se pueden ver en galeria
-y añadir al carrito para la compra.
-Hemos creado varios html y css que se enlazan todos con uno llamado inicio. Tambien hay una carpeta de imagenes y un js.
 
-https://www.figma.com/file/KY2HmEWBPPk3Ic8wpZd5dk/RRICA?type=design&node-id=0%3A1&mode=design&t=CvkF5R7I4H5sdmBf-1
+<div style="text-align:center">
+  <img src="./img/iconos/logo3.svg" alt="Texto Alternativo">
+</div>
 
-https://trello.com/b/7aJRl5Ub
+
+
+
+# Proyecto de Fotografía Miguel Meixoeiro 📸
+¡Ey! Bienvenido a la onda de **Miguel Meixoeiro**, el fotógrafo gallego que está rompiendo esquemas con sus capturas. Este proyecto es la vibra online para que puedas disfrutar de sus fotos en versión imprimible. ¿Listo para sumergirte en el mundo visual de Miguel?
+
+## Estructura del Proyecto 🏗️
+Aquí tienes el mapa del tesoro:
+
+![HTML5](https://img.shields.io/badge/HTML-5-orange?style=flat&logo=html5) La entrada principal a la experiencia. Conoce a Miguel y explora su galería de fotos.
+
+![CSS3](https://img.shields.io/badge/CSS-3-blue?style=flat&logo=css3) El lugar donde la magia visual sucede. Estilo, estilo, estilo.
+
+![JavaScript](https://img.shields.io/badge/JavaScript-yellow?style=flat&logo=javascript) La chispa de la interactividad. ¡Las fotos cobran vida!
+
+images/: Guarda las joyas visuales aquí.
+
+## Diseño en Figma 🎨
+¿Te preguntas cómo se ve este asunto? Echa un vistazo al diseño rebelde en Figma: [Enlace a Figma.](https://www.figma.com/file/vUTdBWlIV7PQVzRoj1TQ27/Grupo-I---Carrito-de-compra?type=design&node-id=73%3A2&mode=design&t=vCJCfo26kunxI0G4-1)
+
+## Despliegue en GitHub 🚀
+El escenario está en GitHub. Haz clic para vivir la experiencia: ¡Explora en GitHub!.
+
+¡Prepárate para ser sorprendido por las imágenes de Miguel! Siéntete libre de clonar, jugar y contribuir. ¡La rebelión visual ha comenzado! 
+
+
+## Instrucciones para Ejecutar el Proyecto 🎉
+
+¡Hora de poner este proyecto en acción sin complicaciones! Sigue estos pasos simples para disfrutar de las fotos de Miguel Meixoeiro:
+
+#### Paso 1: Clona el Repositorio
+Haz clic en el botón verde "Code" en GitHub y copia el enlace del repositorio. Abre tu terminal y utiliza el siguiente comando:
+
+```bash
+Copy code
+git clone https://github.com/DenaRi97/proyectoMiguel.git
+```
+
+#### Paso 2: Abre la Carpeta del Proyecto
+Accede al directorio del proyecto con:
+
+```bash
+Copy code
+cd tuproyecto
+```
+#### Paso 3: Abre el Archivo HTML
+Simplemente, abre el archivo index.html en tu navegador. Puedes hacerlo manualmente o utilizar la terminal:
+```
+bash
+Copy code
+open index.html  # Para sistemas basados en Unix (Linux, macOS)
+start index.html # Para sistemas basados en Windows
+```
+#### Paso 4: ¡Explora y Diviértete!
+¡Eso es todo! Navega por las fotos, conoce a Miguel Meixoeiro y disfruta del proyecto.
+
+¡Listo para rockear con las fotos de Miguel! Si tienes alguna duda, ¡aquí estamos para ayudarte! 🤘
+
--- js/gallery.js
+++ js/gallery.js
@@ -5,7 +5,7 @@ const containerSubCategory = document.querySelector(".gallery_filtersSubCategory
 const buttonSubCategory = document.querySelector(".gallery__buttonSubCategory")
 
 
-//Muestra la API
+//Muestra la API-----------------------------------------------------------------------------
 
 fetch(URL)
     .then(response => {
@@ -22,7 +22,7 @@ fetch(URL)
             const image = document.createElement("img");//añadimos un img para cada foto
             let sourceImage = item.url //llamamos a la ruta de la imagen
             image.src = sourceImage //esto lo hacemos para dejarlo mejor anidado
-            console.log(image.src);
+            // console.log(image.src);
             return image;
 
         }) 
@@ -33,19 +33,39 @@ fetch(URL)
     .catch(error=> console.log('La hemos cagao'))
     
 
+//Funcion para mostrar todas las imagenes
+    function showAllImages(images) { 
+
+        photos.innerHTML = ''
+
+        images.forEach(item => {
+           
+            const imgElement = document.createElement('img');  // Crea un elemento img
+                        
+            imgElement.src = item.url;//coge la ruta de la imagen
+        
+            photos.appendChild(imgElement);// Añade la imagen como hijo de gallery__fotos
+        })
+        
+      }
+    
 
 
-
-
-    //Botones filtros
+//Botones filtros categorías -----------------------------------------------------------
 
 buttonCategory.addEventListener("click", function() {
-    console.log('category');
+    // console.log('category');
 
     
     if (buttonSubCategory.style.display === 'block') { //al hacer clic muestrame las categorias
         buttonSubCategory.style.display = 'none'; //al hacer clic de nuevo desaparecen
         hideAllButtons(containerSubCategory)
+
+        fetch(URL)
+            .then(response => response.json())
+            .then(data => showAllImages(data))
+            .catch(error => console.error('Houston tenemos un problema', error))
+        
 
     } else {
         buttonSubCategory.style.display = 'block';
@@ -63,8 +83,68 @@ function hideAllButtons(container) { //funcion para esconder los botones
 
 function showAllButtons(container) { //funcion para mostrar todos los botones
     
-    const buttons = container.querySelectorAll("button");
-    buttons.forEach(button => {
+    const Allbuttons = container.querySelectorAll("button");
+    Allbuttons.forEach(button => {
         button.style.display = 'block';
     });
 }
+
+
+
+
+
+
+
+//Filtrar API por subcategorías -------------------------------------------------------------------------
+    
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const buttons = document.querySelectorAll('.buttonSubCategory'); //botones subcategoria
+    const galleryFotosElement = document.querySelector('.gallery__fotos')
+
+    
+    buttons.forEach(button => {
+        button.addEventListener('click', function(event) { //creamos un evento
+        
+        // console.clear()
+        const keyword = event.currentTarget.getAttribute('data-keyword'); //cogemos el valor del boton sobre el que hemos hecho clic
+            
+        fetch(URL)
+          .then(response => response.json())
+          .then(data => {
+            
+            const matchingItems = data.filter(item => item.keyword === keyword);
+            // Usamos la funcion filter para crear un nuevo array (filterItems) que contiene los elementos del array original cuyo campo 'keyword' coincide con la keyword del boton
+
+            if (matchingItems.length > 0) { //comprueba si hay coincidencias
+              console.log('Resultados para', keyword, ':', matchingItems);
+              showAllImages(matchingItems) //muestra las img en pantalla
+
+            }             
+          })
+          .catch(error => console.error('Houston tenemos un problema:', error));
+      });
+    });
+
+});
+   
+
+ 
+  
+
+
+
+
+
+//Chuleta para mostrar los objetos del Array
+ // for(let i=0; i < data.length; i++) { //recorre el array
+            // //    console.log(data[i].keyword)
+            //    if(data[i].keyword === `${personas}`){
+            //     console.log(data[i])
+            //    }
+               
+            // }
+
+
+
+
